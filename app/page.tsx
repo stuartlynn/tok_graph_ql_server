76012e49ea027a1a13badb60a93b8e46956c2889
Stuart Lynn <stuart.lynn@gmail.com>
2023-06-09 18:21:26 +0100

    Using nextjs link

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h2>
-        <a href="/api/graphql">TOK Graphql Api Server</a>
+        <Link href="/api/graphql">TOK Graphql Api Server</Link>
       </h2>
     </main>
   );
