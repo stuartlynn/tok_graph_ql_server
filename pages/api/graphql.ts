6c734095b199423115b5016bf9482ce7f2739f62
Stuart Lynn <stuart.lynn@gmail.com>
2023-06-11 14:07:29 +0100

    Trying to get explorer working in production

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,10 +1,28 @@
-import { ApolloServer } from "@apollo/server";
+import { ApolloServer, ApolloServerPlugin } from "@apollo/server";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
 import { neoSchema } from "../../apollo/schema";
 
+import {
+  ApolloServerPluginLandingPageLocalDefault,
+  ApolloServerPluginLandingPageProductionDefault,
+} from "@apollo/server/plugin/landingPage/default";
+
+let plugins: Array<ApolloServerPlugin> = [];
+plugins = [ApolloServerPluginLandingPageLocalDefault({ embed: true })];
+// if (process.env.NODE_ENV === "production") {
+//   plugins = [
+//     ApolloServerPluginLandingPageProductionDefault({
+//       embed: true,
+//       graphRef: "tok@prod",
+//     }),
+//   ];
+// } else {
+//   plugins = [ApolloServerPluginLandingPageLocalDefault({ embed: true })];
+// }
+
 const server = async (): Promise<ApolloServer> => {
   const schema = await neoSchema.getSchema();
-  return new ApolloServer({ schema });
+  return new ApolloServer({ schema, plugins });
 };
 
 export default startServerAndCreateNextHandler(await server());
